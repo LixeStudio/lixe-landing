059fa593de6870793a6bc3beab9699d7c07b0165
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-05-10 17:07:42 +0200

    feat: create header dropdown component

---
+++ src/components/HeaderDropdown.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+import '../assets/scss/components/header-dropdown.scss';
+
+const isDropdownOpened = ref(false);
+</script>
+
+<template>
+  <div class="header__dropdown">
+    <div class="header__dropdown-button" @click="isDropdownOpened = !isDropdownOpened">
+      <div class="header__dropdown-item">
+        <span>eng</span>
+      </div>
+      <div
+        class="header__dropdown-icon"
+        :class="{
+          'header__dropdown-icon_active': isDropdownOpened
+        }"
+      >
+        <img src="../assets/images/icons/chevron-down.svg" alt="Chevron down" />
+      </div>
+    </div>
+    <div
+      class="header__dropdown-content"
+      :class="{
+        'header__dropdown-content_active': isDropdownOpened
+      }"
+    >
+      <div class="header__dropdown-items">
+        <div class="header__dropdown-line"></div>
+        <div class="header__dropdown-item"><span>esp</span></div>
+        <div class="header__dropdown-item"><span>rus</span></div>
+      </div>
+    </div>
+  </div>
+</template>
