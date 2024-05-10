43c85d29bd1ed602f1dfe125534fb37852b4a997
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-05-10 17:07:04 +0200

    feat: create header component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<script setup></script>
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+</script>
 
 <template>
-  <div class="wrapper"></div>
+  <div class="wrapper">
+    <AppHeader />
+  </div>
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script setup>
+import '../assets/scss/components/header.scss';
+import ButtonPrimary from '../components/ButtonPrimary.vue';
+import HeaderDropdown from '../components/HeaderDropdown.vue';
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__container">
+      <div class="header__left-part">
+        <img src="../assets/images/icons/logo.svg" alt="Lixe logo" />
+      </div>
+      <div class="header__right-part">
+        <HeaderDropdown />
+        <div class="header__button-wrapper">
+          <ButtonPrimary />
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
