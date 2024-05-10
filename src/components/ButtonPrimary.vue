2e010f6fa7f23f2c9491364bbf1c723f6eede407
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-05-10 17:07:23 +0200

    feat: add button primary component

---
+++ src/components/ButtonPrimary.vue
@@ -0,0 +1,14 @@
+<script setup>
+import '../assets/scss/components/button-primary.scss';
+</script>
+
+<template>
+  <div class="button-primary">
+    <div class="button-primary__inner-wrapper">
+      <div class="button-primary__inner">
+        <span class="button-primary__text">Get in touch</span>
+        <span class="button-primary__text">Get in touch</span>
+      </div>
+    </div>
+  </div>
+</template>
