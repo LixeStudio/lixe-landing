53cef7e2b4c964a46966f2dc0d085857084a2aa8
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-05-12 10:57:38 +0200

    feat: add hero section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import AppHeader from './components/AppHeader.vue';
+import HeroSection from './components/HeroSection.vue';
 </script>
 
 <template>
   <div class="wrapper">
     <AppHeader />
+    <main>
+      <HeroSection />
+    </main>
   </div>
 </template>
