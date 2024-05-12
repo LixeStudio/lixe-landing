9798da545d5cd7343fb33bf558b3ed343b26e3ff
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-05-12 11:00:23 +0200

    feat: add hero list component

---
+++ src/components/HeroList.vue
@@ -0,0 +1,17 @@
+<script setup>
+import '../assets/scss/components/hero-list.scss';
+</script>
+
+<template>
+  <ul class="hero__list">
+    <li class="hero__list-item">
+      <img src="../assets/images/icons/ellipse.svg" alt="Ellipse" /> <span>Innovation</span>
+    </li>
+    <li class="hero__list-item">
+      <img src="../assets/images/icons/ellipse.svg" alt="Ellipse" /> <span>Web Solutions</span>
+    </li>
+    <li class="hero__list-item">
+      <img src="../assets/images/icons/ellipse.svg" alt="Ellipse" /> <span>Functionality</span>
+    </li>
+  </ul>
+</template>
