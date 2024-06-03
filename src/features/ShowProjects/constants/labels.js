a8fcb7829db1ec70615ed4361e282702f0580d31
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-03 14:09:58 +0200

    refactor: rename tabs to labels

---
+++ src/features/ShowProjects/constants/labels.js
@@ -0,0 +1,10 @@
+export const labels = [
+  'Web Development',
+  'Design',
+  'Mobile App',
+  'UX/UI',
+  'orci sagittis magna',
+  'euismod',
+  'purus arcu tellus',
+  'purus arcu'
+];
---
+++ src/shared/ui/BaseLabel/BaseLabel.vue
@@ -0,0 +1,29 @@
+<script setup>
+import './base-label.scss';
+
+defineProps({
+  selected: {
+    type: Boolean,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <button
+    type="button"
+    class="label"
+    :class="{
+      label_selected: selected
+    }"
+  >
+    <div class="label__container">
+      <p class="label__title"><slot /></p>
+      <sup class="label__number">{{ quantity }}</sup>
+    </div>
+  </button>
+</template>
