91a2fd06012a52560750eb2a91fea01ba909f029
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-05 21:24:29 +0200

    feat: add new colors variables

--- src/shared/constants/colors.js
+++ src/shared/constants/colors.js
@@ -1,4 +1,8 @@
 export const colors = {
   white: '#ffffff',
-  dark: '#111928'
+  dark: '#111928',
+  greenLight: '#daf8e6',
+  greenLark: '#1a8245',
+  redLight: '#fef3f3',
+  redDark: '#bc1c21'
 };
