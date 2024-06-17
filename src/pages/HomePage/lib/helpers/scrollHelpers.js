cad2a2f75b3b65a1867dc28ce60c14b2d92348f7
SLEEPY\Maryia <mariaguchkova1@gmail.com>
2024-06-17 08:47:17 +0200

    fix: add padding right only for desktop

--- src/pages/HomePage/lib/helpers/scrollHelpers.js
+++ src/pages/HomePage/lib/helpers/scrollHelpers.js
@@ -1,9 +1,18 @@
+function isMobile() {
+  const userAgent = navigator.userAgent;
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|Tablet|tablet/i.test(
+    userAgent
+  );
+}
+
 export const hideScroll = () => {
   document.body.style.overflow = 'hidden';
-  document.body.style.paddingRight = `0.625rem`;
+  if (!isMobile()) {
+    document.body.style.paddingRight = '0.625rem';
+  }
 };
 
 export const displayScroll = () => {
   document.body.style.overflow = 'auto';
-  document.body.style.paddingRight = `0rem`;
+  document.body.style.paddingRight = '0rem';
 };
